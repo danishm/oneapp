62bf0b68b658f3bd5a1f724c895764bd915bdd66
Danish Mujeeb <danish.mujeeb@gmail.com>
2015-07-19 11:25:57 -0400

    Added a few details on the apps

--- data.py
+++ data.py
@@ -1,7 +1,28 @@
 applications = [
     {
+        'key': 'facebook',
         'name': 'Facebook',
-        'url': 'https://m.facebook.com/',
-        'icon': ''
+        'url': 'https://m.facebook.com/'
+    },
+    {
+        'key': 'twitter',
+        'name': 'Twitter',
+        'url': 'https://mobile.twitter.com/'
+    },
+    {
+        'key': 'linkedin',
+        'name': 'LinkedIn',
+        'url': 'https://touch.www.linkedin.com/?rs=false&redirectUrl=%23stream&ahoy=no#stream'
+    },
+    {
+        'key': 'google-maps',
+        'name': 'Google Maps',
+        'url': 'https://www.google.com/maps/'
+    },
+    {
+        'key': 'gmail',
+        'name': 'Gmail',
+        'url': 'https://www.google.com/maps/'
     }
+
 ]
